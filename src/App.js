cc637982b3ea7d14a60e601ee636c019c5ce1a7e
Nazar.M <inazar.m@gmail.com>
2020-06-17 22:39:21 +0500

    Updated App.Js
    Some changes in app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          <span>Changes made by Nazar.</span>
         </p>
         <a
           className="App-link"
